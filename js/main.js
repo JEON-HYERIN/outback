3584e1b52b52c4fe26297ac5796f996b4d797d2d
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-23 22:08:37 +0900

    d

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const mainMenuLists = document.querySelectorAll('#gnb ul li');
-const active = document.querySelector('#gnb ul li.active');
+// const active = document.querySelector('#gnb ul li.active');
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
 
 mainMenuLists.forEach(function (mainMenuList) {
   mainMenuList.addEventListener('mouseenter', function (event) {
-    active.classList.remove('active');
+    // active.classList.remove('active');
     event.target.classList.add('active');
   });
   mainMenuList.addEventListener('mouseleave', function (event) {
-    active.classList.add('active');
+    // active.classList.add('active');
     event.target.classList.remove('active');
   });
 });
