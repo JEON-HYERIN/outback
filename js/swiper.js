6e4be09363161df8ebad6a729985e65eac6e53fe
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-24 21:32:58 +0900

    CSS: mobile - product

--- js/swiper.js
+++ js/swiper.js
@@ -12,8 +12,17 @@ new Swiper('#visual .swiper', {
 });
 
 new Swiper('#main .product .swiper', {
-  autoplay: true,
+  // autoplay: true,
   loop: true,
+  breakpoints: {
+
+    320: {
+      slidesPerView: 1,
+    },
+    850.1: {
+      slidesPerView: 2,
+    }
+  },
   slidesPerView: 2,
   // centeredSlides: true,
   navigation: {
