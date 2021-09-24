2c3c8b3c71566e827e6ae920955a71ae4c81e268
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-24 20:13:59 +0900

    ARROW UP!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// gnb영역 이벤트 
 const mainMenuLists = document.querySelectorAll('#gnb ul li');
 // const active = document.querySelector('#gnb ul li.active');
 
@@ -15,3 +16,32 @@ mainMenuLists.forEach(function (mainMenuList) {
     event.target.classList.remove('active');
   });
 });
+
+// ARROW-UP 버튼 클릭시 상단이동
+const visual = document.querySelector('#visual');
+const visualHeight = visual.getBoundingClientRect().height;
+const arrowUpBtn = document.querySelector('#arrow-up');
+const wrapper = document.querySelector('#wrapper');
+arrowUpBtn.addEventListener('click', function () {
+  wrapper.scrollIntoView({behavior:"smooth"});
+});
+
+// 헤더영역 
+const header = document.querySelector('#header');
+window.addEventListener('scroll', function () {
+  if(window.pageYOffset > 6) {
+    header.classList.add('red');
+  } else {
+    header.classList.remove('red');
+  };
+
+  if(window.pageYOffset > visualHeight / 2) {
+    arrowUpBtn.classList.remove('invisible');
+  } else {
+    arrowUpBtn.classList.add('invisible');
+  }
+
+});
+
+
+
