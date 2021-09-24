1241db99de2d8d71665296943a135fb0109a5a31
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-24 13:35:19 +0900

    CSS: PRODUCT

--- js/swiper.js
+++ js/swiper.js
@@ -10,3 +10,18 @@ new Swiper('#visual .swiper', {
     clickable: true
   }
 });
+
+new Swiper('#main .product .swiper', {
+  autoplay: true,
+  loop: true,
+  slidesPerView: 2,
+  // centeredSlides: true,
+  navigation: {
+    prevEl: '.swiper-prev',
+    nextEl: '.swiper-next'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
