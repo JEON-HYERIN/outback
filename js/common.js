da6a6ce0350f31965ea1aa07f3fc4a27e8474fd0
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-10-25 16:18:08 +0900

    gnb focus, footer 수정

--- js/common.js
+++ js/common.js
@@ -4,6 +4,10 @@ $(document).on('click', 'a[href="#"]', function (e) {
   e.preventDefault();
 });
 
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
 // gnb영역 이벤트 
 const mainMenu = document.querySelector('#gnb > ul');
 const subMenues = mainMenu.querySelectorAll('.sub-menu');
@@ -11,39 +15,15 @@ const mainMenuLists = document.querySelectorAll('#gnb > ul > li');
 const anchers = document.querySelectorAll('#gnb > ul > li > a');
 const icons = mainMenu.querySelectorAll('span.material-icons');
 
-if (window.NodeList && !NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = Array.prototype.forEach;
-}
+$('#gnb > ul > li > a').on('mouseenter focusin', function () {
+  var index = $('#gnb > ul > li').index($(this).parent());
+  $('#gnb > ul > li > .sub-menu').removeClass('on');
+  $('#gnb > ul > li:eq(' + index + ') > .sub-menu').addClass('on');
+});
 
-// 모바일버전 서브메뉴 오픈
-// mainMenu.addEventListener('click', function (event) {
-
-//   mainMenuLists.forEach(function (event) {
-//     // console.log(event.target)
-//   });
-//   subMenues.forEach(function (subMenu) {
-//     // console.dir(event.target)
-//     const link = event.target.dataset.link || event.target.parentElement.dataset.link;
-//     if ((link === subMenu.dataset.filter)) {
-//       if (event.target.localName === 'li') {
-//         event.target.classList.toggle('active');
-//       } else if (event.target.localName === 'a') {
-//         event.target.parentElement.classList.add('active');
-//       }
-
-//       subMenu.classList.toggle('open');
-
-//       icons.forEach(function (icon) {
-//         if (subMenu.dataset.filter === icon.dataset.name) {
-//           icon.classList.toggle('rotate');
-//         }
-//       });
-//     } else {
-//       return;
-//     }
-//   });
-// });
-
+$('#header').on('mouseleave', function () {
+  $('#gnb > ul > li > .sub-menu').removeClass('on');
+});
 
 // 모바일버전 서브메뉴 오픈
 $('#gnb > ul > li > a').on('click', function () {
@@ -60,29 +40,6 @@ const wrapper = document.querySelector('#wrapper');
 arrowUpBtn.addEventListener('click', function () {
   wrapper.scrollIntoView({behavior:"smooth"});
 });
-
-// 토글버튼 활성화
-// const openBtn = document.querySelector('.open-btn');
-// const closeBtn = document.querySelector('.close-btn');
-// const gnbEl = document.querySelector('#gnb');
-// const topMenu = document.querySelector('#header .top-menu');
-
-// openBtn.addEventListener('click', function () {
-//   header.classList.add('black');
-//   header.classList.remove('red');
-//   gnbEl.classList.add('open');
-//   topMenu.classList.add('open');
-//   openBtn.classList.add('hidden');
-//   closeBtn.classList.remove('hidden');
-// });
-
-// closeBtn.addEventListener('click', function () {
-//   header.classList.remove('black');
-//   gnbEl.classList.remove('open');
-//   topMenu.classList.remove('open');
-//   openBtn.classList.remove('hidden');
-//   closeBtn.classList.add('hidden');
-// });
 
 // 헤더영역 
 const header = document.querySelector('#header');
