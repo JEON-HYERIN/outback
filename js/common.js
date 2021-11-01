a8fc484249c6d8c56dd078e5276480380af2b7fd
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-11-01 15:14:18 +0900

    mobile header수정

--- js/common.js
+++ js/common.js
@@ -26,12 +26,46 @@ $('#header').on('mouseleave', function () {
 });
 
 // 모바일버전 서브메뉴 오픈
-$('#gnb > ul > li > a').on('click', function () {
-  var index = $('#gnb > ul > li').index($(this).parent());
-  $('#gnb > ul > li').removeClass('active');
-  $('#gnb > ul > li:eq(' + index + ')').toggleClass('active');
-  $('#gnb > ul > li:eq(' + index + ') > .sub-menu').toggleClass('open');
-  $('#gnb > ul > li:eq(' + index + ') > a > .material-icons').toggleClass('rotate');
+// $('#gnb > ul > li > a').on('click', function () {
+//   var index = $('#gnb > ul > li').index($(this).parent());
+//   $('#gnb > ul > li').removeClass('active');
+//   $('#gnb > ul > li:eq(' + index + ')').toggleClass('active');
+//   $('#gnb > ul > li:eq(' + index + ') > .sub-menu').toggleClass('open');
+//   $('#gnb > ul > li:eq(' + index + ') > a > .material-icons').toggleClass('rotate');
+// });
+
+$('#gnb > ul > li').each(function () {
+  if ($(this).find('ul').length > 0) {
+    $(this).find('> a').append('<span class="material-icons mobile" data-name="brand">navigate_next</span>');
+  }
+});
+
+$('#header .open-btn').on('click', function () {
+  $('#header .open-btn').addClass('hidden');
+  $('#header .close-btn').removeClass('hidden');
+  $('#gnb > ul > li.active > a').trigger('click');
+});
+
+$('#gnb > ul > li > a').on('click', function (e) {
+  if ($(window).width() < 1024) {
+    if ($(this).parent().find('ul').length > 0) {
+      e.preventDefault();
+      var height = 0;
+      $(this).next().find('> li').each(function () {
+        height += $(this).outerHeight(true);
+      });
+      $(this).next().css({'height': height + 'px'});
+      $(this).find('span.material-icons').addClass('rotate');
+      $(this).parent().siblings().find('ul').css({'height': '0px'});
+      $(this).parent().siblings().find('span.material-icons').removeClass('rotate');
+    }
+  }
+});
+
+$(window).on('resize', function () {
+  if ($(window).width() >= 1024) {
+    $('#gnb > ul > li > ul').removeAttr('style');
+  }
 });
 
 // TO-TOP 버튼 클릭시 상단이동
