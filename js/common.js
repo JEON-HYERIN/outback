c2f21c1cc680e9651ac29269995712e01d7db822
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-11-05 21:31:32 +0900

    js파일수정

--- js/common.js
+++ js/common.js
@@ -8,6 +8,36 @@ if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
 
+const body = document.querySelector('body');
+var scrollPosition = 0;
+// 모달창 오픈
+function enable() {
+  scrollPosition = window.pageYOffset;
+  body.style.overflow = 'hidden';
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollPosition}px`;
+  body.style.width = '100%';
+}
+// 모달창 닫기
+function disable() {
+  body.style.removeProperty('overflow');
+  body.style.removeProperty('position');
+  body.style.removeProperty('top');
+  body.style.removeProperty('width');
+  window.scrollTo(0, scrollPosition);
+}
+
+// 헤더영역 
+const header = document.querySelector('#header');
+const headerHeight = header.getBoundingClientRect().height;
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > headerHeight) {
+    toTopBtn.classList.remove('invisible');
+  } else {
+    toTopBtn.classList.add('invisible');
+  }
+});
+
 // gnb영역 이벤트 
 const mainMenu = document.querySelector('#gnb > ul');
 const subMenues = mainMenu.querySelectorAll('.sub-menu');
@@ -16,9 +46,11 @@ const anchers = document.querySelectorAll('#gnb > ul > li > a');
 const icons = mainMenu.querySelectorAll('span.material-icons');
 
 $('#gnb > ul > li > a').on('mouseenter focusin', function () {
-  var index = $('#gnb > ul > li').index($(this).parent());
-  $('#gnb > ul > li > .sub-menu').removeClass('on');
-  $('#gnb > ul > li:eq(' + index + ') > .sub-menu').addClass('on');
+  if ($(window).width() >= 1024) {
+    var index = $('#gnb > ul > li').index($(this).parent());
+    $('#gnb > ul > li > .sub-menu').removeClass('on');
+    $('#gnb > ul > li:eq(' + index + ') > .sub-menu').addClass('on');
+  }
 });
 
 $('#header').on('mouseleave', function () {
@@ -26,14 +58,6 @@ $('#header').on('mouseleave', function () {
 });
 
 // 모바일버전 서브메뉴 오픈
-// $('#gnb > ul > li > a').on('click', function () {
-//   var index = $('#gnb > ul > li').index($(this).parent());
-//   $('#gnb > ul > li').removeClass('active');
-//   $('#gnb > ul > li:eq(' + index + ')').toggleClass('active');
-//   $('#gnb > ul > li:eq(' + index + ') > .sub-menu').toggleClass('open');
-//   $('#gnb > ul > li:eq(' + index + ') > a > .material-icons').toggleClass('rotate');
-// });
-
 $('#gnb > ul > li').each(function () {
   if ($(this).find('ul').length > 0) {
     $(this).find('> a').append('<span class="material-icons mobile" data-name="brand">navigate_next</span>');
@@ -44,9 +68,16 @@ $('#header .open-btn').on('click', function () {
   $('#header .open-btn').addClass('hidden');
   $('#header .close-btn').removeClass('hidden');
   $('#gnb > ul > li.active > a').trigger('click');
+  enable();
 });
 
-$('#gnb > ul > li > a').on('click', function (e) {
+$('#header .close-btn').on('click', function () {
+  $('#header .open-btn').removeClass('hidden');
+  $('#header .close-btn').addClass('hidden');
+  disable();
+});
+
+$('#gnb > ul > li > a').on('click focusin', function (e) {
   if ($(window).width() < 1024) {
     if ($(this).parent().find('ul').length > 0) {
       e.preventDefault();
@@ -65,8 +96,11 @@ $('#gnb > ul > li > a').on('click', function (e) {
 $(window).on('resize', function () {
   if ($(window).width() >= 1024) {
     $('#gnb > ul > li > ul').removeAttr('style');
-  } else {
-    $('#gnb > ul > li.active > a').trigger('click');
+    if ($('#gnb').hasClass('open') === true) {
+      disable();
+    } 
+  } else if ($('#gnb').hasClass('open') === true){
+    $('#header .open-btn').trigger('click');
   }
 });
 
@@ -77,15 +111,6 @@ toTopBtn.addEventListener('click', function () {
   wrapper.scrollIntoView({behavior:"smooth"});
 });
 
-// 헤더영역 
-const header = document.querySelector('#header');
-const headerHeight = header.getBoundingClientRect().height;
-window.addEventListener('scroll', function () {
-  if (window.pageYOffset > headerHeight) {
-    toTopBtn.classList.remove('invisible');
-  } else {
-    toTopBtn.classList.add('invisible');
-  }
-});
+
 
 
